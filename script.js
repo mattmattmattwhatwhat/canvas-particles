5e572323a430333a4b7ee9d6a3fb06817e39ec74
mattmattmattwhatwhat <matt.thompson0@gmail.com>
2013-10-12 11:31:19 -0400

    added animation toggle

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var canvas = null,
 	ctx = null;
 
 var animating = false,
-	framerate = 1000/60,
+	framerate = 1000/10,
 	animationInterval = null;
 
 $(document).ready(function() {
@@ -13,13 +13,30 @@ function initialize() {
 	canvas = document.getElementById("maincanvas");
 	ctx = canvas.getContext('2d');
 	resizeCanvas(window.innerWidth, window.innerHeight);
-	animationInterval = setInterval(animateScene, framerate);
-	animating = true;
+	toggleAnimation();
 }
 
 function resizeCanvas(width, height) {
 	canvas.w = width;
 	canvas.h = height;
+}
+
+function animateScene() {
+	clearCanvas();
+	drawCircle(100, 100, 50, "#0000FF");
+}
+
+function toggleAnimation() {
+	if (animating) {
+		animationInterval = null;
+	} else {
+		animationInterval = setInterval(animateScene, framerate);
+	}
+	animating = !animating;
+}
+
+function clearCanvas() {
+	ctx.clearRect(0, 0, canvas.w, canvas.h);
 }
 
 function drawCircle(x, y, r, color) {
@@ -29,11 +46,3 @@ function drawCircle(x, y, r, color) {
 	ctx.fill();
 }
 
-function clearCanvas() {
-	ctx.clearRect(0, 0, canvas.w, canvas.h);
-}
-
-function animateScene() {
-	clearCanvas();
-	drawCircle(100, 100, 50, "#0000FF");
-}
