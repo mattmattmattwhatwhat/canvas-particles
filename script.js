6fab25b489628bf24c2b87ff1830d3ed5c4eb973
mattmattmattwhatwhat <matt.thompson0@gmail.com>
2013-10-12 11:27:17 -0400

    added animation backbone

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var canvas = null;
-var ctx = null;
+var canvas = null,
+	ctx = null;
+
+var animating = false,
+	framerate = 1000/60,
+	animationInterval = null;
 
 $(document).ready(function() {
 	initialize();
@@ -9,7 +13,8 @@ function initialize() {
 	canvas = document.getElementById("maincanvas");
 	ctx = canvas.getContext('2d');
 	resizeCanvas(window.innerWidth, window.innerHeight);
-	//drawCircle(100, 100, 50, "rgba(10,100,200,1)");
+	animationInterval = setInterval(animateScene, framerate);
+	animating = true;
 }
 
 function resizeCanvas(width, height) {
@@ -26,4 +31,9 @@ function drawCircle(x, y, r, color) {
 
 function clearCanvas() {
 	ctx.clearRect(0, 0, canvas.w, canvas.h);
+}
+
+function animateScene() {
+	clearCanvas();
+	drawCircle(100, 100, 50, "#0000FF");
 }
