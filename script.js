e1e7a629245d624c207779124c1c6c23ccf3426c
mattmattmattwhatwhat <matt.thompson0@gmail.com>
2013-10-12 11:11:08 -0400

    removed doc margins, added drawCircle/resizeCanvas functions

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 var canvas = null;
 var ctx = null;
 
-function initialize() {
-	canvas = document.getElementById("maincanvas");
-	ctx = canvas.getContext('2d');
-}
-
 $(document).ready(function() {
 	initialize();
 });
+
+function initialize() {
+	canvas = document.getElementById("maincanvas");
+	ctx = canvas.getContext('2d');
+	resizeCanvas(window.innerWidth, window.innerHeight);
+	//drawCircle(100, 100, 50, "rgba(10,100,200,1)");
+}
+
+function resizeCanvas(width, height) {
+	canvas.w = width;
+	canvas.h = height;
+}
+
+function drawCircle(x, y, r, color) {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2*Math.PI, false);
+	ctx.fillStyle = color;
+	ctx.fill();
+}
